aa95c6b925b151ce176360105aae0c1ef2eccf20
fulvaz <fulvaz@foxmail.com>
2019-01-10 19:43:02 +0800

    fix git case issue 2

--- src/Components/IconFont/IconFont.tsx
+++
@@ -1,5 +0,0 @@
-import { Icon } from 'antd';
-
-export const Iconfont = Icon.createFromIconfontCN({
-  scriptUrl: "./assets/font_327949_wzau780vocj.js"
-});
--- src/Components/PageLayout/PageTitle.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export class PageTitle extends React.Component {
-  public render() {
-    return <React.Fragment>{this.props.children}</React.Fragment>;
-  }
-}
