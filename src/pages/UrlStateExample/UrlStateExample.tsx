5b407b5e1150f4b7781599942d441ed04c271b9e
kround <yuan404kang@126.com>
2019-01-18 13:50:58 +0800

    还在优化中

--- src/pages/UrlStateExample/UrlStateExample.tsx
+++ src/pages/UrlStateExample/UrlStateExample.tsx
@@ -1,115 +1,112 @@
-import React, { Component } from "react";
-import { PageLayout } from "../../components/PageLayout/PageLayout";
-import { PageTitle } from "../../components/PageLayout/PageTitle";
-import { PageContent } from "../../components/PageLayout/PageContent";
-import { Table } from "antd";
-import axios from "../../utils/request";
-import { changeUrl, putStateInUrl } from "../../utils/urlState";
+import React, { Component } from 'react';
+import { PageLayout } from '../../components/PageLayout/PageLayout';
+import { PageTitle } from '../../components/PageLayout/PageTitle';
+import { PageContent } from '../../components/PageLayout/PageContent';
+import { Table } from 'antd';
+import axios from '../../utils/request';
+import { changeUrl, putStateInUrl } from '../../utils/urls';
 
 const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    sorter: true,
-    render: name => `${name.first} ${name.last}`,
-    width: "20%"
-  },
-  {
-    title: "Gender",
-    dataIndex: "gender",
-    filters: [
-      { text: "Male", value: "male" },
-      { text: "Female", value: "female" }
-    ],
-    width: "20%"
-  },
-  {
-    title: "Email",
-    dataIndex: "email"
-  }
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        sorter: true,
+        render: (name) => `${name.first} ${name.last}`,
+        width: '20%'
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        filters: [ { text: 'Male', value: 'male' }, { text: 'Female', value: 'female' } ],
+        width: '20%'
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email'
+    }
 ];
 
-@putStateInUrl({ table: { api: "fetch" } })
+@putStateInUrl('table', { fetch: [ 'pagination', 'filters', 'sorter' ] })
 export class UrlStateExample extends Component {
-  state = {
-    data: [],
-    pagination: {
-      current: 1,
-      total: 0,
-      pageSize: 10
-    },
-    filters: {},
-    sorter: {},
-    loading: false
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount: emit when query string change");
-    this.fetch();
-  }
-
-  @changeUrl({
-    namespace: "table",
-    filters: ["pagination", "filters", "sorter"]
-  })
-  handleTableChange(pagination, filters, sorter) {
-    this.state = {
-      ...this.state,
-      pagination,
-      filters,
-      sorter
-    };
-    // this.fetch();
-    // 这里不可以在放fetch, 否则会发两次请求
-
-  }
-
-  fetch(_ = {}) {
-    const { pagination, sorter } = this.state;
-    const params = {
-      results: pagination.pageSize,
-      page: pagination.current,
-      sortField: (sorter as any).field,
-      sortOrder: (sorter as any).order
+    state = {
+        data: [],
+        pagination: {
+            current: 1,
+            total: 0,
+            pageSize: 10
+        },
+        filters: {},
+        sorter: {},
+        loading: false
     };
 
-    // console.log("params:", params);
-    // this.setState({ loading: true });
-    // axios({
-    //   url: "https://randomuser.me/api",
-    //   method: "get",
-    //   params: {
-    //     results: 10,
-    //     ...params
-    //   }
-    // }).then(res => {
-    //   const pagination = { ...this.state.pagination };
-    //   // Read total count from server
-    //   // pagination.total = data.totalCount;
-    //   pagination.total = 200;
-    //   this.setState({
-    //     loading: false,
-    //     data: (res.data as any).results,
-    //     pagination
-    //   });
-    // });
-  }
+    componentDidMount() {
+        console.log('componentDidMount: emit when query string change');
+        // this.fetch();
+    }
 
-  render() {
-    return (
-      <PageLayout ifBackShow={true}>
-        <PageTitle>这是一个标题</PageTitle>
-        <PageContent>
-          <Table
-            columns={columns}
-            rowKey={record => record.login.uuid}
-            dataSource={this.state.data}
-            pagination={this.state.pagination}
-            loading={this.state.loading}
-            onChange={this.handleTableChange.bind(this)}
-          />
-        </PageContent>
-      </PageLayout>
-    );
-  }
+    @changeUrl({
+        namespace: 'table',
+        filters: [ 'pagination', 'filters', 'sorter' ]
+    })
+    handleTableChange(pagination, filters, sorter) {
+        this.state = {
+            ...this.state,
+            pagination,
+            filters,
+            sorter
+        };
+        // this.fetch();
+        // 这里不可以在放fetch, 否则会发两次请求
+    }
+
+    fetch(cb: any) {
+        console.log(cb);
+        // const { pagination, sorter } = this.state;
+        // const params = {
+        //     results: pagination.pageSize,
+        //     page: pagination.current,
+        //     sortField: (sorter as any).field,
+        //     sortOrder: (sorter as any).order
+        // };
+
+        // console.log('params:', params);
+        // this.setState({ loading: true });
+        // axios({
+        //     url: 'https://randomuser.me/api',
+        //     method: 'get',
+        //     params: {
+        //         results: 10,
+        //         ...params
+        //     }
+        // }).then((res) => {
+        //     const pagination = { ...this.state.pagination };
+        //     // Read total count from server
+        //     // pagination.total = data.totalCount;
+        //     pagination.total = 200;
+        //     this.setState({
+        //         loading: false,
+        //         data: (res.data as any).results,
+        //         pagination
+        //     });
+        // });
+    }
+
+    render() {
+        return (
+            <PageLayout ifBackShow={true}>
+                <PageTitle>这是一个标题</PageTitle>
+                <PageContent>
+                    <Table
+                        columns={columns}
+                        rowKey={(record) => record.login.uuid}
+                        dataSource={this.state.data}
+                        pagination={this.state.pagination}
+                        loading={this.state.loading}
+                        onChange={this.handleTableChange.bind(this)}
+                    />
+                </PageContent>
+            </PageLayout>
+        );
+    }
 }
---
+++ src/utils/urls.ts
@@ -0,0 +1,143 @@
+import {
+    ParamsToParamsObj,
+    ParamsObjToObject,
+    JudgeParamsStatus,
+    CreateParamsByFilters,
+    ObjectToParamsObj,
+    AssignObject
+} from './urlsTool';
+
+const DEFAULT_STATE_KEY = '__defaultState';
+
+interface PutStateInUrlConfig {
+    [method: string]: string[];
+}
+
+interface ChangeUrlConfig {
+    namespace: string;
+    filters: string[];
+    isDav?: boolean;
+}
+
+export function putStateInUrl(namespace: string, config: PutStateInUrlConfig, iSDav?: boolean): any {
+    return function(target): any {
+        const componentDidMount = target.prototype.componentDidMount;
+
+        //组件声明周期只在初始化时调用一次，自定义method一定会被调用一次
+        target.prototype.componentDidMount = function(...args) {
+            //根据isDav来决定保存的初始状态来自props或者state
+            this[DEFAULT_STATE_KEY] = JSON.parse(JSON.stringify(iSDav ? this.props : this.state));
+            const { location: { search: currSearch } } = this.props;
+            const currUrlParams = new URLSearchParams(currSearch);
+
+            //将url参数转化 paramsObj格式 {namespace-filter-a-b...-e-f:value}
+            const curr_paramsObj = ParamsToParamsObj(currUrlParams);
+
+            Object.keys(config).forEach((method: string) => {
+                const filters = config[method];
+
+                //将paramsObj格式转为childObj格式 {namespace-filter-a-b...-e-f} to {filter:{a:{b:obj}}}
+                const paramsToChildObj = ParamsObjToObject(curr_paramsObj, namespace);
+
+                //将url参数值与初始值合并生成state
+                const state = AssignObject(paramsToChildObj, this[DEFAULT_STATE_KEY]);
+
+                //获取跟filters相关的值
+                const params = CreateParamsByFilters(state, filters);
+                const setStateCb = () => {
+                    const remoteReqMethodName = method || `fetch${method}`;
+                    this[remoteReqMethodName].call(this, params);
+                };
+
+                //非Dav环境会帮忙设置state并调用method，Dav环境不会设置state只会调用method
+                if (!iSDav) {
+                    this.state = {
+                        ...this.state,
+                        ...state
+                    };
+                    setStateCb();
+                } else {
+                    setStateCb();
+                }
+            });
+            componentDidMount.apply(this, args);
+        };
+
+        //组件改变就会调用
+        const componentDidUpdate = target.prototype.componentDidUpdate;
+        target.prototype.componentDidUpdate = function(prevProps, prevState, snapshot) {
+            console.log('2222')
+            const { location: { search: prevSearch } } = prevProps;
+            const { location: { search: currSearch } } = this.props;
+            const prevUrlParams = new URLSearchParams(prevSearch);
+
+            //将旧的url参数转化 paramsObj格式 {namespace-filter-a-b...-e-f:value}
+            const prev_paramsObj = ParamsToParamsObj(prevUrlParams);
+            const currUrlParams = new URLSearchParams(currSearch);
+
+            //将新的url参数转化 paramsObj格式 {namespace-filter-a-b...-e-f:value}
+            const curr_paramsObj = ParamsToParamsObj(currUrlParams);
+
+            Object.keys(config).forEach((method: string) => {
+
+                //标记url参数是否改变
+                let status: boolean = false;
+                const filters = config[method];
+                filters.forEach((filter: string) => {
+                    if (!status) {
+                        status = JudgeParamsStatus(namespace, filter, curr_paramsObj, prev_paramsObj);
+                    }
+                });
+                if (status) {
+
+                    //将paramsObj格式转为childObj格式 {namespace-filter-a-b...-e-f} to {filter:{a:{b:obj}}}
+                    const paramsToChildObj = ParamsObjToObject(curr_paramsObj, namespace);
+
+                    //将url参数值与state/props值合并生成新的state
+                    const state = AssignObject(paramsToChildObj, iSDav ? this.props : this.state);
+
+                    //获取跟filters相关的值
+                    const params = CreateParamsByFilters(state, filters);
+                    const setStateCb = () => {
+                        const remoteReqMethodName = method || `fetch${method}`;
+                        this[remoteReqMethodName].call(this, params);
+                    };
+
+                    //非Dav环境会帮忙设置state并调用method，Dav环境不会设置state只会调用method
+                    if (!iSDav) {
+                        // this.setState(
+                        //     {
+                        //         ...state
+                        //     },
+                        //     setStateCb
+                        // );
+                        setStateCb();
+                    } else {
+                        setStateCb();
+                    }
+                }
+            });
+            if (componentDidUpdate) {
+                componentDidUpdate.call(this, prevProps, prevState, snapshot);
+            }
+        };
+    };
+}
+
+export function changeUrl(config: ChangeUrlConfig) {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+        const method = descriptor.value;
+        descriptor.value = function(...args) {
+            method.apply(this, args);
+            const urlParams = new URLSearchParams();
+            config.filters.forEach((filter) => {
+                const params = ObjectToParamsObj(config.isDav ? this.props : this.state, config.namespace, filter);
+                console.log(params);
+                Object.keys(params).forEach((param) => {
+                    urlParams.set(param, params[param]);
+                });
+            });
+            this.props.history.push(`?${urlParams}`);
+        };
+    };
+}
